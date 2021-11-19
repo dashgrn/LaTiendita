c3cc134f01fd38889266771eb89cc2531a4947cf
Jose Fernandez <josesk20@gmail.com>
2021-11-19 09:37:14 -0500

    inital state

---
+++ src/router/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter as Router, 
+    Routes,
+    Route} 
+    from 'react-router-dom';
+
+
+export const AppRouter = () => {
+    return (
+        <div>
+            this is the main router
+        </div>
+    )
+}
